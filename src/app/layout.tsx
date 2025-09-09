f805ef71f336f626d2bb70e46a18ea177c7d382b
priyankaamba67-sketch <priyankaamba67@gmail.com>
2025-09-09 16:31:26 +0530

    document fix remaining

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    // <ClerkProvider>
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
@@ -47,6 +47,6 @@ export default function RootLayout({
           </NuqsAdapter>
         </body>
       </html>
-    // </ClerkProvider>
+    </ClerkProvider>
   );
 }
